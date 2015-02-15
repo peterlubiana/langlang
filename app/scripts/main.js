731183a3ae05507610d067a62a886f824f135e55
Peter Lubiana <peterlubiana@gmail.com>
2015-02-15 03:18:06 +0100

    Added the files that make up the foundation of the app.

---
+++ app/scripts/main.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+var x = {
+
+	initApp:function(){
+
+
+		x.hideNShow();
+		x.bindHandlers();
+
+	},
+
+	
+
+	hideStartMenu:function(){
+		$('.jumbotron').hide();
+		
+	},
+
+	hideNShow:function(){
+		$("#app-navigation").hide();
+		$("#learningboard").hide();
+	},
+
+	getRandomCharacter:function(){
+
+		var t = this;
+		var chars = [new t.CharacterModel('我',"I / me, character refering to 1. person","wǒ"),
+					 new t.CharacterModel('人',"person / human","rén"),
+					 new t.CharacterModel('你',"you, character refering to 2. person","nǐ")];
+
+					 var rand = Math.round(Math.random()*(chars.length-1));
+
+		return  chars[rand];
+
+	},
+
+
+	// This function can be used to flip around the language data
+	// depending on if the users wants to learn characters or guess tones or
+	// whatever, using a string flag..
+	pushCharacterToBoard:function(chardata){
+		var d = chardata;
+
+		// Build up html for the data.
+		var html_char = "<span class='board_char'>"+d.char+"</span>";
+		var html_translation ="<span class='board_translation'>"+d.translation+"</span>";
+		var html_pinyin = "<span class='board_pinyin'>"+d.pinyin+"</span>"
+		var $html = html_char+html_pinyin+html_translation;
+		    
+		    
+
+		$("#learningboard").html($html);
+	},
+
+
+	/*****
+	*
+	*		MODELS FOR DATA...
+	*
+	*
+	* ** ** * *** * **/
+
+	// A SIMPLE MODEL FOR A CHARACTER.
+	CharacterModel:function(char,translation,pinyin){
+		var obj = {};
+		obj.char = char;
+		obj.translation = translation;
+		obj.pinyin = pinyin;
+		return obj;
+	},
+
+	/**
+	*
+	*		HANDLERS FOR BUTTONS / UI.
+	*
+	* **** **
+	* **** ** **  **/
+
+
+	bindHandlers:function(){
+		var t = this;
+		$(document).on('click','#startbutton',function(){
+			t.hideStartMenu();
+			var data = t.getRandomCharacter();
+			$("#app-navigation").show();
+
+			t.pushCharacterToBoard(data);
+			$("#learningboard").show();
+		});
+
+		$(document).on("click","#appnav_nextbutton",function(){
+			var data = t.getRandomCharacter();
+			t.pushCharacterToBoard(data);
+		});
+
+	},
+
+};
+
+
+
+
+
+
+
+
+
+// JQUERY BOILERPLATE.
+$(document).ready(function(){
+	x.initApp();
+});
